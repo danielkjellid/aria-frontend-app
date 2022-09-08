d879395911ebc0ae131bc159e612050707860cc3
Daniel Kjellid <daniel.kjellid@gmail.com>
2022-09-08 17:53:59 +0200

    Make m2 units a bit prettier

---
+++ components/Product/UnitTag.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+interface ProductUnitTagProps {
+  unit: string
+}
+
+defineProps<ProductUnitTagProps>()
+</script>
+
+<template>
+  <span v-if="unit === 'm2'" class="text-gray-500">m<sup>2</sup></span>
+  <span v-else class="text-gray-500">{{ unit }}</span>
+</template>
