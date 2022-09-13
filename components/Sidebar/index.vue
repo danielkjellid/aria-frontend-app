b6db17887c41e1a98b9fa7ddc2124a9546e3d455
Daniel Kjellid <daniel.kjellid@gmail.com>
2022-09-13 10:01:45 +0200

    Start on admin

---
+++ components/Sidebar/index.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-72 sticky top-0 bottom-0 left-0 flex flex-col flex-none h-screen"></div>
+</template>
---
+++ layouts/admin.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <main class="h-screen">
+      <Sidebar />
+    </main>
+  </div>
+</template>
